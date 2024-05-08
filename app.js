a9b06105b4d7df09e28193ac46ef45e3c8ca8fec
aleksandramuraveva <84822235+aleksandramuraveva@users.noreply.github.com>
2024-05-08 21:33:43 +0300

    Add slider for projects and change style

--- app.js
+++ app.js
@@ -28,3 +28,31 @@ document.addEventListener("mouseout", () => {
 })
 
 //cursor ends
+
+
+//Swiper JS
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+  speed: 1000,
+
+  // // If we need pagination
+  // pagination: {
+  //   el: '.swiper-pagination',
+  // },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+});
+
+//Swiper JS ends
