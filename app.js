4ebe27f14f2a5a4af50b9667305c168ffc1009bd
aleksandramuraveva <84822235+aleksandramuraveva@users.noreply.github.com>
2024-05-11 02:44:55 +0300

    Add changing language feature

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const cursor = document.querySelector(".cursor");
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
-//cursor
+
+
+//CURSOR
+
 let timeout;
 
 document.addEventListener("mousemove", (e) => {
@@ -27,21 +30,51 @@ document.addEventListener("mouseout", () => {
 	cursor.style.display = "none";
 })
 
-//cursor ends
+//CURSOR ends
+
+
+//Changing language of the site
+
+console.log("hm");
+
+function loadLanguage(lang) {
+  fetch(`${lang}.json`)
+    .then(response => response.json())
+    .then(translations => {
+    	 console.log(translations);
+      document.querySelectorAll('[data-i18n]').forEach(el => {
+
+      
+        const key = el.getAttribute('data-i18n');
+        	console.log(translations[key]);
+
+        el.textContent = translations[key];
+
+      });
+    })
+    .catch(error => console.error('Error loading the language file:', error));
+}
+
+//Test
+// loadLanguage('en');
+let langSelector = document.getElementById('language-selector');
+
+langSelector.addEventListener('change', function(e) {
+  loadLanguage(e.target.value);
+  console.log("it works");
+});
+
+
+
+//Changing language of the site ends
 
 
 //Swiper JS
 
 const swiper = new Swiper('.swiper', {
-  // Optional parameters
   direction: 'vertical',
   loop: true,
   speed: 1000,
-
-  // // If we need pagination
-  // pagination: {
-  //   el: '.swiper-pagination',
-  // },
 
   // Navigation arrows
   navigation: {
